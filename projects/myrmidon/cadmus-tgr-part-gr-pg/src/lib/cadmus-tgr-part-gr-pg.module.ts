65b25890f4a30c73bae3b3cd720f8d39edcfb34c
Myrmex <dfusi@hotmail.com>
2020-11-22 16:29:13 +0100

    Module

--- projects/myrmidon/cadmus-tgr-part-gr-pg/src/lib/cadmus-tgr-part-gr-pg.module.ts
+++ projects/myrmidon/cadmus-tgr-part-gr-pg/src/lib/cadmus-tgr-part-gr-pg.module.ts
@@ -1,7 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { PendingChangesGuard } from '@myrmidon/cadmus-core';
-import { LING_TAGS_FRAGMENT_TYPEID } from '@myrmidon/cadmus-tgr-part-gr-ui';
+import { CadmusCoreModule, PendingChangesGuard } from '@myrmidon/cadmus-core';
+import { CadmusMaterialModule } from '@myrmidon/cadmus-material';
+import { CadmusStateModule } from '@myrmidon/cadmus-state';
+import {
+  CadmusTgrPartGrUiModule,
+  LING_TAGS_FRAGMENT_TYPEID,
+} from '@myrmidon/cadmus-tgr-part-gr-ui';
+import { CadmusUiModule } from '@myrmidon/cadmus-ui';
+import { CadmusUiPgModule } from '@myrmidon/cadmus-ui-pg';
 import { LingTagsFragmentFeatureComponent } from './ling-tags-fragment-feature/ling-tags-fragment-feature.component';
 
 export const RouterModuleForChild = RouterModule.forChild([
@@ -16,7 +25,17 @@ export const RouterModuleForChild = RouterModule.forChild([
 @NgModule({
   declarations: [LingTagsFragmentFeatureComponent],
   imports: [
-    RouterModuleForChild
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    // Cadmus
+    RouterModuleForChild,
+    CadmusCoreModule,
+    CadmusMaterialModule,
+    CadmusStateModule,
+    CadmusUiModule,
+    CadmusUiPgModule,
+    CadmusTgrPartGrUiModule,
   ],
   exports: [LingTagsFragmentFeatureComponent],
 })
