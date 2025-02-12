bc97c7dbb27fcf1e24391780ca7ca7f295e4cec6
Myrmex <dfusi@hotmail.com>
2025-02-12 15:49:30 +0100

    Updated Angular and packages

--- public/env.js
+++ public/env.js
@@ -4,7 +4,7 @@
 
   // environment-dependent settings
   window.__env.apiUrl = "http://localhost:59590/api/";
-  window.__env.version = "9.0.0";
+  window.__env.version = "9.0.1";
   // enable thesaurus import in thesaurus list for admins
   window.__env.thesImportEnabled = true;
 })(this);
